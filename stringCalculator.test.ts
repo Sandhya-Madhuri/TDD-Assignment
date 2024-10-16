08855aeb98ef3f9a72cd87d047aa3d468e540f42
sandhya madhuri <101086309+Sandhya-Madhuri@users.noreply.github.com>
2024-10-16 20:52:17 +0530

    STEP2: Add two or more numbers and added other unit testcases

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -1,12 +1,27 @@
 import StringCalculator from "./stringCalculator";
 
+let calculator: StringCalculator;
+
+beforeEach(() => {
+  calculator = new StringCalculator();
+});
+
 describe("String Calculator", () => {
   test("should return 0 for an empty string", () => {
-    const calculator = new StringCalculator();
     expect(calculator.add("")).toBe(0);
   });
-  test('should return 1 when the input is "1"', () => {
-    const calculator = new StringCalculator();
-    expect(calculator.add("1")).toBe(1);
+  test('should return 100 when the input is "1"', () => {
+    expect(calculator.add("100")).toBe(100);
   });
+  test('should return 3 when the input is "1,2"', () => {});
+  test('should return 35 when the input is "1,2,7,5,6,3,2,9"', () => {
+    expect(calculator.add("1,2,7,5,6,3,2,9")).toBe(35);
+  });
+
+  test.todo('should return 6 when the input is "1\\n2,3"');
+  test.todo(
+    'should return 6 when using a custom delimiter ";" with input "//;\\n1;2;3"'
+  );
+  test.todo("should throw exception for negative numbers");
+  test.todo("should throw exception for multiple negative numbers");
 });
--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,6 +1,10 @@
 export default class StringCalculator {
   add(numbers: string): number {
     if (numbers === "") return 0;
+    if (numbers.includes(",")) {
+      const numsArray = numbers.split(",").map((num) => parseInt(num, 10));
+      return numsArray.reduce((sum, num) => sum + num, 0);
+    }
     return parseInt(numbers);
   }
 }
