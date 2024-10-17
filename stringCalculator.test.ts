454b389f7a4ec867ae9b47f27c80015dfa1a8236
sandhya madhuri <101086309+Sandhya-Madhuri@users.noreply.github.com>
2024-10-17 18:21:23 +0530

    STEP3: Add two or more numbersseperated by comma or newline

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -10,15 +10,23 @@ describe("String Calculator", () => {
   test("should return 0 for an empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
-  test('should return 100 when the input is "1"', () => {
+
+  test('should return 100 when the input is "100"', () => {
     expect(calculator.add("100")).toBe(100);
   });
-  test('should return 3 when the input is "1,2"', () => {});
+
+  test('should return 3 when the input is "1,2"', () => {
+    expect(calculator.add("1,2")).toBe(3);
+  });
+
   test('should return 35 when the input is "1,2,7,5,6,3,2,9"', () => {
     expect(calculator.add("1,2,7,5,6,3,2,9")).toBe(35);
   });
 
-  test.todo('should return 6 when the input is "1\\n2,3"');
+  test('should return 10 when the input is "1\\n2,3,4"', () => {
+    expect(calculator.add("1\n2,3,4")).toBe(10);
+  });
+
   test.todo(
     'should return 6 when using a custom delimiter ";" with input "//;\\n1;2;3"'
   );
