b4af837d6deda49f504d9af51b773af6dbeb01ec
sandhya madhuri <101086309+Sandhya-Madhuri@users.noreply.github.com>
2024-10-17 18:21:13 +0530

    STEP3: Add two or more numbersseperated by comma or newline

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,10 +1,10 @@
 export default class StringCalculator {
   add(numbers: string): number {
     if (numbers === "") return 0;
-    if (numbers.includes(",")) {
-      const numsArray = numbers.split(",").map((num) => parseInt(num, 10));
-      return numsArray.reduce((sum, num) => sum + num, 0);
-    }
-    return parseInt(numbers);
+    const delimiterRegex = /[\n,]+/;
+    const numsArray = numbers
+      .split(delimiterRegex)
+      .map((num) => parseInt(num, 10));
+    return numsArray.reduce((sum, num) => sum + num, 0);
   }
 }
